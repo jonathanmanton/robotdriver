aec97a865ec53f599875f785f0de880bb7165a83
Jonathan Manton <jonathan@manton.com>
2024-05-14 03:01:11 +0100

    Got skeleton working and compiling

--- svc/mc_node.c
+++ svc/mc_node.c
@@ -7,6 +7,13 @@
 #include <std_msgs/msg/int32.h>
 #include <rmw_microros/rmw_microros.h>
 
+#include "robotdriver_interfaces/srv/motor_effort.h"
+#include "robotdriver_interfaces/srv/motor_velocity.h"
+#include "robotdriver_interfaces/srv/pid_gains.h"
+#include "robotdriver_interfaces/msg/controller_status.h"
+#include "robotdriver_interfaces/msg/motor_status.h"
+#include <std_srvs/srv/empty.h>
+
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
 
@@ -19,9 +26,27 @@ node_data nd;
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
 
+rcl_publisher_t RightMotorStatus_publisher;
+rcl_publisher_t LeftMotorStatus_publisher;
+rcl_publisher_t RightControllerStatus_publisher;
+rcl_publisher_t LeftControllerStatus_publisher;
+robotdriver_interfaces__msg__MotorStatus RightMotorStatus_msg;
+robotdriver_interfaces__msg__MotorStatus LeftMotorStatus_msg;
+robotdriver_interfaces__msg__ControllerStatus RightControllerStatus_msg;
+robotdriver_interfaces__msg__ControllerStatus LeftControllerStatus_msg;
+
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
     rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+	RightMotorStatus_msg.motor_position = msg.data + 6;
+	RightMotorStatus_msg.motor_velocity = msg.data / 3.14;
+	RightMotorStatus_msg.motor_effort = msg.data * 3.14;
+
+	ret += rcl_publish(&RightMotorStatus_publisher, &RightMotorStatus_msg, NULL);
+/*	rcl_publish(&RightControllerStatus_publisher, &RightControllerStatus_msg, NULL);
+	rcl_publish(&LeftMotorStatus_publisher, &LeftMotorStatus_msg, NULL);
+	rcl_publish(&LeftControllerStatus_publisher, &LeftControllerStatus_msg, NULL);
+*/
     msg.data++;
 }
 
@@ -47,6 +72,21 @@ int main()
     }
 
 	init_node_data (&nd, "pico_node");
+
+    rclc_publisher_init_default(
+        &RightMotorStatus_publisher,
+        &(nd.node),
+        ROSIDL_GET_MSG_TYPE_SUPPORT(robotdriver_interfaces, msg, MotorStatus),
+        "right_motor_status");
+
+/*
+    rclc_publisher_init_default(
+        &LeftMotorStatus_publisher,
+        &(nd.node),
+        ROSIDL_GET_MSG_TYPE_SUPPORT(robotdriver_interfaces, msg, MotorStatus),
+        "left_motor_status");
+*/
+
     rclc_publisher_init_default(
         &publisher,
         &(nd.node),
